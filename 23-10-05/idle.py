daa2ff8da61cbf8cb2db68a93d2611c9541cb564
Marcin Majewski <skilletssgames@gmail.com>
2023-10-05 08:51:49 +0200

    Add files via upload

---
+++ 23-10-05/idle.py
@@ -0,0 +1,20 @@
+Python 3.10.7 (tags/v3.10.7:6cc6b13, Sep  5 2022, 14:08:36) [MSC v.1933 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print("hi")
+hi
+>>> print "hello"
+SyntaxError: Missing parentheses in call to 'print'. Did you mean print(...)?
+>>> print("hello")
+hello
+>>> 50 % 4
+2
+>>> 50 // 3
+16
+>>> 10 // 2
+5
+>>> 10 / 2
+5.0
+>>> 7 // 3
+2
+>>> 7/3
+2.3333333333333335
